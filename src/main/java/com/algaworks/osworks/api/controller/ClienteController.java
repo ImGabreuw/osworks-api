6cc07854207cfc8b83800df001dc24a564229ee7
ImGabreuw <gabrielkazama9@gmail.com>
2021-01-13 15:08:09 -0300

    Create ClienteRepository.java

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -2,25 +2,22 @@ package com.algaworks.osworks.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
-
-	@PersistenceContext
-	private EntityManager manager;
+	
+	@Autowired
+	private ClienteRepository repository;
 
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager
-				.createQuery("FROM Cliente", Cliente.class)
-				.getResultList();
+		return repository.findAll();
 	}
 
 }
--- src/main/resources/db/migration/V001__cria-tabela-cliente.sql
+++ src/main/resources/db/migration/V001__cria-tabela-cliente.sql
@@ -1,4 +1,4 @@
-create table client (
+create table cliente (
 	id bigint not null auto_increment,
     nome varchar(60) not null,
     email varchar(255) not null,
