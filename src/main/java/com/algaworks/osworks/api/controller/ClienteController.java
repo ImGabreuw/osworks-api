7b40ade4f1bdb07483010a45067512502014b703
ImGabreuw <gabrielkazama9@gmail.com>
2021-01-13 15:37:25 -0300

    Implementation of adicionar method

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
@@ -28,7 +32,7 @@ public class ClienteController {
 				.findAll();
 	}
 	
-	@GetMapping("/{id}")
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long id) {
 		Function<Cliente, ResponseEntity<Cliente>> foundedCliente = cliente -> ResponseEntity
 				.ok()
@@ -41,6 +45,12 @@ public class ClienteController {
 				.findById(id)
 				.map(foundedCliente)
 				.orElse(notFoundedCliente);
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return repository.save(cliente);
 	}
 
 }
