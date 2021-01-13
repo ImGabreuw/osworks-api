727b1c6958aa18405aff0e4ec1d808de72205110
ImGabreuw <gabrielkazama9@gmail.com>
2021-01-13 15:42:02 -0300

    Implementation of  method

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -9,8 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -51,6 +53,22 @@ public class ClienteController {
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return repository.save(cliente);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+		if (repository.existsById(id)) {
+			return ResponseEntity
+					.notFound()
+					.build();
+		}
+		
+		cliente.setId(id);
+		cliente = repository.save(cliente);
+		
+		return ResponseEntity
+				.ok()
+				.body(cliente);
 	}
 
 }
