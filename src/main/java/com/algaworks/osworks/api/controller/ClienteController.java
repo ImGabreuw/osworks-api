c9530589389ca75a27f74519fa4ca88b80065397
ImGabreuw <gabrielkazama9@gmail.com>
2021-01-13 15:29:45 -0300

    Implementation of buscar method

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,23 +1,46 @@
 package com.algaworks.osworks.api.controller;
 
 import java.util.List;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
 	@Autowired
 	private ClienteRepository repository;
 
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
-		return repository.findAll();
+//		return repository.findByNome("João da Silva); Busca pelo nome informado
+//		return repository.findByNomeContaining("Si"); Busca pelo nome com base na String informada
+		return repository
+				.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long id) {
+		Function<Cliente, ResponseEntity<Cliente>> foundedCliente = cliente -> ResponseEntity
+				.ok()
+				.body(cliente);
+		ResponseEntity<Cliente> notFoundedCliente = ResponseEntity
+				.notFound()
+				.build();
+		
+		return repository
+				.findById(id)
+				.map(foundedCliente)
+				.orElse(notFoundedCliente);
 	}
 
 }
