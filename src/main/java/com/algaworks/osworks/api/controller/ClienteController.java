c5f79b555319cd356f4410ecf28618f52708b46d
ImGabreuw <gabrielkazama9@gmail.com>
2021-01-14 09:43:21 -0300

    Implementation of remover method and data validation

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -6,6 +6,7 @@ import java.util.function.Function;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +69,21 @@ public class ClienteController {
 		return ResponseEntity
 				.ok()
 				.body(cliente);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> remover(@PathVariable Long id) {
+		if (!repository.existsById(id)) {
+			return ResponseEntity
+					.notFound()
+					.build();
+		}
+		
+		repository.deleteById(id);
+		
+		return ResponseEntity
+				.noContent()
+				.build();
 	}
 
 }
--- src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Cliente implements Serializable {
@@ -16,8 +19,17 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank //Não aceita: null / "" / "      " 
+	@Size(max = 60)
 	private String nome;
+	
+	@NotBlank
+	@Email //Validação do formato do email
+	@Size(max = 255)
 	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
 	private String telefone;
 	
 	public Cliente() {
