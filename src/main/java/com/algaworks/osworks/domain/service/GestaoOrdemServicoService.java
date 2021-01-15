2222be7ebd18bd56b7053ad4f457ff41c8a9dc08
ImGabreuw <gabrielkazama9@gmail.com>
2021-01-15 10:55:05 -0300

    Fix bugs

--- src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
+++ src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
@@ -1,6 +1,6 @@
 package com.algaworks.osworks.domain.service;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,7 +27,7 @@ public class GestaoOrdemServicoService {
 		
 		ordemServico.setCliente(cliente);
 		ordemServico.setStatus(StatusOrdemServico.ABERTA);
-		ordemServico.setDataAbertura(LocalDateTime.now());
+		ordemServico.setDataAbertura(OffsetDateTime.now());
 		
 		return repositorServicoRepository.save(ordemServico);
 	}
