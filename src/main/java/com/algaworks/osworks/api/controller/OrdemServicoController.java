40ae24300f37f9b1bcf68812debe767fea7a0b72
ImGabreuw <gabrielkazama9@gmail.com>
2021-01-15 10:36:18 -0300

    implementation of listar method - Get all OrdemServicos from database

--- src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
+++ src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
@@ -1,9 +1,12 @@
 package com.algaworks.osworks.api.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.OrdemServico;
+import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 import com.algaworks.osworks.domain.service.GestaoOrdemServicoService;
 
 @RestController
@@ -19,11 +23,18 @@ public class OrdemServicoController {
 
 	@Autowired
 	private GestaoOrdemServicoService service;
+	@Autowired
+	private OrdemServicoRepository repository;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico) {
 		return service.criar(ordemServico);
+	}
+	
+	@GetMapping
+	public List<OrdemServico> listar() {
+		return repository.findAll();
 	}
 	
 }
